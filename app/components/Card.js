6208ff6497bff9d5a264b06cb3dec9d6e5c19451
Charles Wefso <cwefso@gmail.com>
2021-01-11 16:31:55 -0700

    creates flip handler and conditional rendering

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import CardFlip from "react-native-card-flip";
 // import Flippy, { FrontSide, BackSide } from "react-flippy";
 
 const Card = ({ id, chamber }) => {
 	const [member, setMember] = useState({});
 	const [imageURL, setImageUrl] = useState("");
+	const [isFlipped, setIsFlipped] = useState(false);
 
 	useEffect(() => {
 		loadMember();
 		setImageUrl(`https://theunitedstates.io/images/congress/450x550/${id}.jpg`);
 	}, []);
+
+	const handleFlip = () => {
+		isFlipped === false ? setIsFlipped(true) : setIsFlipped(false);
+	};
 
 	const loadMember = () => {
 		var myHeaders = new Headers();
@@ -38,81 +44,75 @@ const Card = ({ id, chamber }) => {
 			return <Text> - {committee.name}</Text>;
 		});
 
-		return (
-			<View className="App-header">
-				<View>
-					<View className="container">
-						<View className="profile">
+		if (isFlipped === false) {
+			return (
+				<View className="App-header">
+					<View>
+						<TouchableOpacity style={styles.card} onPress={handleFlip}>
+							{/* CardFront Open */}
 							<Image
 								source={{ uri: imageURL }}
 								style={{ width: 200, height: 300 }}
 								alt="profile-Image"
 							/>
-						</View>
+							{/* CardFront Close */}
+						</TouchableOpacity>
 					</View>
 				</View>
-				{chamber === "House" && (
-					<View>
-						<View className="container">
-							<View className="profile">
-								{/* <img src={imageURL} alt="profile-img"></img> */}
-								<View className="committees">
-									<Text>
-										Name: Rep. {member.first_name} {member.last_name}
-									</Text>
-									<Text>Party: {member.roles[1].party}</Text>
-									<Text>
-										State: ({member.roles[1].state}-{member.roles[1].district})
-									</Text>
-									<Text>Leadership Role: {leadership}</Text>
-									<Text>Committees:</Text>
-									<View>{committees}</View>
-									<Text>
-										{/* <a
-														href={`https://www.opensecrets.org/search?q=${member.first_name}+${member.last_name}&type=indiv`}
-														style={{ color: "white" }}
-													>
-														Open Secrets Search
-													</a> */}
-									</Text>
-								</View>
-							</View>
-						</View>
-					</View>
-				)}
-				{chamber === "Senate" && (
-					<View>
-						<View>
-							<View className="profile">
-								{/* <img src={imageURL} alt="profile-img"></img> */}
-								<View className="committees">
-									<View>
-										<Text>
-											Name: Sen. {member.first_name} {member.last_name}
-										</Text>
-										<Text>Party: {member.roles[1].party}</Text>
-										<Text>State: {member.roles[1].state}</Text>
-										<Text>Leadership Role: {leadership}</Text>
-										<Text>
-											Committees:
-											<View>{committees}</View>
-										</Text>
-									</View>
-								</View>
-							</View>
-						</View>
-					</View>
-				)}
-			</View>
-		);
-	} else {
-		console.log("Loading");
-		return (
-			<View>
-				<Text>Loading...</Text>
-			</View>
-		);
+			);
+		} else if (isFlipped === true && chamber === "House") {
+				return (
+					<TouchableOpacity style={styles.card} onPress={handleFlip}>
+						<Text>
+							Name: Rep. {member.first_name} {member.last_name}
+						</Text>
+						<Text>Party: {member.roles[1].party}</Text>
+						<Text>
+							State: ({member.roles[1].state}-{member.roles[1].district})
+						</Text>
+						<Text>Leadership Role: {leadership}</Text>
+						<Text>Committees:</Text>
+						<View>{committees}</View>
+						<Text>
+							{/* <a
+													href={`https://www.opensecrets.org/search?q=${member.first_name}+${member.last_name}&type=indiv`}
+													style={{ color: "white" }}
+												>
+													Open Secrets Search
+												</a> */}
+						</Text>
+					</TouchableOpacity>
+				);
+												}	else if (isFlipped === true && chamber === "Senate") {
+				return (
+					<TouchableOpacity style={styles.card} onPress={handleFlip}>
+						<Text>
+							Name: Sen. {member.first_name} {member.last_name}
+						</Text>
+						<Text>Party: {member.roles[1].party}</Text>
+						<Text>State: {member.roles[1].state}</Text>
+						<Text>Leadership Role: {leadership}</Text>
+						<Text>
+							Committees:
+							<View>{committees}</View>
+						</Text>
+					</TouchableOpacity>
+				);
+			}
+		} else {
+			console.log("Loading");
+			return (
+				<View>
+					<Text>Loading...</Text>
+				</View>
+			);
+		}
 	}
-};
+;
 
 export default Card;
+
+const styles = {
+	front: {},
+	back: {},
+};
